9c9c393dc3aa2ef61b1cf94f739aa4609a8d0cdf
KakuMatsui <kaku.envart@gmail.com>
2024-01-10 22:43:09 +0900

    Add:　Support for headline highlighting and in-page linking.

---
+++ js/current_header.js
@@ -0,0 +1,32 @@
+//single-contentを記事のクラスとし、記事内のh2、h3のみ取得
+const headingContents = document.querySelectorAll(
+    '.frame-article-middle h2, .frame-article-middle h3')
+
+//各見出しの絶対位置
+let headingPos =  [...headingContents].map(
+    element => Math.floor(element.getBoundingClientRect().top + 
+    window.scrollY))
+
+//サイド目次
+const sideIndex = document.getElementById('article-index')
+//サイド目次の全見出しタイトル
+const sideIndexItem = sideIndex.querySelectorAll('a[href]')
+//見出しの数
+const headingNum = headingContents.length
+//オフセット
+const offset = 300
+
+window.addEventListener('scroll', () => {
+    const currentPos = window.scrollY // 現在値
+    //現在値の判定
+    //見出しiの位置＜スクロール量＜見出しi+1のとき、見出しiにcurrentクラスをつける
+    for (let i = 0; i < headingNum; i++) {
+        if (i < headingNum - 1 && currentPos + offset >= headingPos[i] && currentPos + offset < headingPos[i + 1]) {
+          sideIndexItem[i].classList.add('current')
+        } else if (i === headingNum - 1 && currentPos + offset >= headingPos[i]) {
+          sideIndexItem[i].classList.add('current')
+        } else {
+            sideIndexItem[i].classList.remove('current')
+        }
+      }
+})
